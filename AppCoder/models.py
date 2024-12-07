3f2ddd02057af745f03351d43512d4c3640472a2
Ramiro Gil Leguizamon <ramirogil99@gmail.com>
2024-12-07 18:12:29 -0300

    correcciones y avance

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -1,16 +1,20 @@
 from django.db import models
-
+"""agregar mas detalles a los cursos y a las demas clases como horarios, fechas, mas datos en otras clases"""
 class Curso(models.Model):
     nombre = models.CharField(max_length=100)  # Campo string de 100 caracteres
     comision = models.IntegerField()  # Campo entero
     
     def __str__(self):
-        return f'Nombre del curso: {self.nombre}, comision: {self.comision}'
+        return f'Nombre del curso: {self.nombre} | comision: {self.comision}'
 
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=30)  # Campo string de 100 caracteres
     apellido = models.CharField(max_length=30)  # Campo string de 100 caracteres
     email = models.EmailField()  # Campo de email
+    
+    def __str__(self):
+        return f'Estudiante: {self.apellido}, {self.nombre} | e-mail: {self.email}'
+
 
 class Profesor(models.Model):
     nombre = models.CharField(max_length=30)  # Campo string de 30 caracteres
@@ -19,10 +23,13 @@ class Profesor(models.Model):
     profesion = models.CharField(max_length=50)  # Campo string de 50 caracteres
     
     def __str__(self):
-        return f"Profesor: {self.nombre} - Curso: {self.profesion}"
+        return f"Profesor: {self.apellido}, {self.nombre} | Curso: {self.profesion}"
 
 class Entregable(models.Model):
     nombre = models.CharField(max_length=100)  # Campo string de 100 caracteres
     fechaDeEntrega = models.DateField()  # Campo de fecha
     entregado = models.BooleanField()  # Campo booleano
+    
+    def __str__(self):
+        return f"Alumno: {self.nombre} | Fecha de entrega: {self.fechaDeEntrega} | Entregado: {self.entregado}"
 
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -2,22 +2,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Curso
+from .models import Curso, Profesor, Entregable, Estudiante
 
 def curso(request):
-    curso = Curso.objects.all()
+    curso = Curso.objects.all() #Esto se obtienen los atributos completos de la clase
     print(curso)
     
-    return render(request, 'AppCoder/cursos.html') #Es el renderizado para las templates de la seccion Curso
+    return render(request, 'AppCoder/cursos.html', {'cursos':curso})#Es el renderizado para las templates de la seccion Curso
 
 def inicio(request):
     return render(request, 'AppCoder/index.html') #Es el renderizado para las templates de la seccion Inicio
 
 def profesor(request):
-    return render(request, 'AppCoder/profesores.html') #Es el renderizado para las templates de la seccion profesores
+    profesor = Profesor.objects.all() #Esto se obtienen los atributos completos de la clase
+    print(profesor)
+    
+    return render(request, 'AppCoder/profesores.html', {'profesor':profesor}) #Es el renderizado para las templates de la seccion profesores
 
 def estudiante(request):
-    return render(request, 'AppCoder/estudiantes.html') #Es el renderizado para las templates de la seccion estudiantes
+    estudiante = Estudiante.objects.all() #Esto se obtienen los atributos completos de la clase
+    print(estudiante)
+    
+    return render(request, 'AppCoder/estudiantes.html', {'estudiante':estudiante}) #Es el renderizado para las templates de la seccion estudiantes
 
-def entregable(request):
-    return render(request, 'AppCoder/entregables.html') #Es el renderizado para las templates de la seccion entregables
+def entregable(request):    
+    entregable = Entregable.objects.all() #Esto se obtienen los atributos completos de la clase
+    print(entregable)
+    
+    return render(request, 'AppCoder/entregables.html', {'entregable':entregable}) #Es el renderizado para las templates de la seccion entregables
